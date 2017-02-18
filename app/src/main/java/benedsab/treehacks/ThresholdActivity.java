087f3b502a889a5492bb5eccee7c0740d4c18d1f
sbeleuz <sbeleuz@stanford.edu>
2017-02-18 05:08:49 -0800

    modified ThresholdActivity

--- app/src/main/java/benedsab/treehacks/ThresholdActivity.java
+++ app/src/main/java/benedsab/treehacks/ThresholdActivity.java
@@ -1,19 +1,31 @@
 package benedsab.treehacks;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ThresholdActivity extends AppCompatActivity implements ShakeDetector.OnShakeListener{
+public class ThresholdActivity extends AppCompatActivity {
 
     @Override
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_threshold);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+            @Override
+            public void onShake(int count) {
 
-    }
-
-    @Override
-    public void onShake(int count) {
-
+            }
+        });
     }
 }
+
+
