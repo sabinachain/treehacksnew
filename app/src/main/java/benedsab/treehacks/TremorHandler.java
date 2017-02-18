b997e0ececfc42437c35d62c6837c219d3e3e9c8
Edward Lee <edlee1@stanford.edu>
2017-02-18 02:23:57 -0800

    Changed TremorHandler from IntentService to Service

---
+++ app/src/main/java/benedsab/treehacks/TremorHandler.java
@@ -0,0 +1,16 @@
+package benedsab.treehacks;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TremorHandler extends Service {
+    public TremorHandler() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
