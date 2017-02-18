73c962920c8a24b7b00df554b3c76135fac803bb
sbeleuz <sbeleuz@stanford.edu>
2017-02-18 04:32:19 -0800

    new ThresholdActivity

--- app/src/main/java/benedsab/treehacks/MainActivity.java
+++ app/src/main/java/benedsab/treehacks/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+public static String SHAKE_THRESHOLD = "com.benedsab.treehacks.threshold";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(!getSharedPreferences("threshhold", MODE_PRIVATE).contains(SHAKE_THRESHOLD)){
+
+        }
     }
 }
